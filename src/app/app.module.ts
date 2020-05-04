ecc7d1db4462c54b2ece4cde71d48d3e2bd8b1c0
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-04 18:04:44 -0500

    basic component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideogamesComponent } from './videogames/videogames.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideogamesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/videogames/videogames.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'videogames',
+    template: '<h2>{{name}} component</h2>'
+})
+export class VideogamesComponent{
+    public name='videogames';
+}
