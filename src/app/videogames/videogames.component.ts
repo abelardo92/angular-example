34f1ac005abbe4d3431763b6a85586732762b8d4
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-04 18:27:06 -0500

    'for' example

--- src/app/videogames/videogames.component.ts
+++ src/app/videogames/videogames.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
     <h2>{{name}} component</h2>
     <h2 *ngIf='!show_retro_game'>best game: {{best_game}}</h2>
     <h2 [style.background]='color' *ngIf='show_retro_game'>best retro game: {{best_retro_game}}</h2>
+    <h2>videogames list</h2>
+    <ul>
+        <li *ngFor="let videogame of videogames">{{videogame}}</li>
+    </ul>
     `
 })
 export class VideogamesComponent{
@@ -14,4 +18,7 @@ export class VideogamesComponent{
     public best_retro_game = 'mario';
     public show_retro_game = false;
     public color = 'yellow';
+    public videogames = [
+        'metroid', 'mario', 'pokemon', 'the witcher'
+    ]
 }
