198723ff3fed987ca093e298ffb98ba4859ab7b1
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-04 18:17:03 -0500

    conditional example

--- src/app/videogames/videogames.component.ts
+++ src/app/videogames/videogames.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'videogames',
-    template: '<h2>{{name}} component</h2>'
+    template: `
+    <h2>{{name}} component</h2>
+    <h2 *ngIf='!show_retro_game'>best game: {{best_game}}</h2>
+    <h2 [style.background]='color' *ngIf='show_retro_game'>best retro game: {{best_retro_game}}</h2>
+    `
 })
 export class VideogamesComponent{
-    public name='videogames';
+    public name = 'videogames';
+    public best_game = 'zelda';
+    public best_retro_game = 'mario';
+    public show_retro_game = false;
+    public color = 'yellow';
 }
