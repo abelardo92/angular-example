b0daa6726faee8e8696574a0c120c6d7ba56529d
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-04 21:31:52 -0500

    template file created

--- src/app/videogames/videogames.component.ts
+++ src/app/videogames/videogames.component.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'videogames',
-    template: `
-    <h2>{{name}} component</h2>
-    <h2 *ngIf='!show_retro_game'>best game: {{best_game}}</h2>
-    <h2 [style.background]='color' *ngIf='show_retro_game'>best retro game: {{best_retro_game}}</h2>
-    <h2>videogames list</h2>
-    <ul>
-        <li *ngFor="let videogame of videogames">{{videogame}}</li>
-    </ul>
-    `
+    templateUrl: './videogames.component.html' 
 })
 export class VideogamesComponent{
     public name = 'videogames';
