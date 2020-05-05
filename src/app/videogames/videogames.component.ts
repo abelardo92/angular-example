50d11d3013e2eae5167fa5d56b0ecb70750ccf75
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-04 21:43:11 -0500

    variable types added

--- src/app/videogames/videogames.component.ts
+++ src/app/videogames/videogames.component.ts
@@ -5,12 +5,24 @@ import { Component } from '@angular/core';
     templateUrl: './videogames.component.html' 
 })
 export class VideogamesComponent{
-    public name = 'videogames';
-    public best_game = 'zelda';
-    public best_retro_game = 'mario';
-    public show_retro_game = false;
-    public color = 'yellow';
-    public videogames = [
-        'metroid', 'mario', 'pokemon', 'the witcher'
-    ]
+    public name:string;
+    public best_game:string;
+    public best_retro_game:string;
+    public show_retro_game:boolean;
+    public color:string;
+    public year:number;
+    public videogames:Array<String>;
+
+    constructor() {
+        this.name = 'videogames';
+        this.best_game = 'zelda';
+        this.best_retro_game = 'mario';
+        this.show_retro_game = false;
+        this.color = 'yellow';
+        this.year = 2020;
+        this.videogames = [
+            'metroid', 'mario', 'pokemon', 'the witcher'
+        ];
+    }
+
 }
